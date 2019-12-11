f67cbea498c76e845546353a0fc9e8086196900c
jui patel <amin.jui2712@gmail.com>
2019-12-11 10:34:40 -0500

    updating all file

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+function generate(){
+
+  
+  // var complexity = document.getElementById("slider").value;
+
+  var complexity = document.getElementById("slider").value;
+  console.log(complexity);
+  var values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
+  var password = "";
+
+  
+  for(var i = 0; i <= complexity; i++){
+      password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  }
+
+  
+  document.getElementById("display").value = password;
+
+  
+
+}
+
+
+function copyPassword(){
+
+  document.getElementById("display").select();
+
+  document.execCommand("Copy");
+
+  alert("Password copied to clipboard!");
+
+}
